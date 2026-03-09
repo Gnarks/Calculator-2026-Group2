67dc2afc5c93053f344efc16a5350863d08838c3
antoine <antoine@delobbe.be>
2026-03-09 19:43:40 +0100

    feat: added the web based GUI of the calculator

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import Calculator from './components/Calculator.vue';
 </script>
 
 <template>
-  <HelloWorld msg="test-props"/>
+  <Calculator />
 </template>
 
-<style scoped>
-
+<style>
+body {
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: hsl(0, 0%, 95%);
+}
 </style>

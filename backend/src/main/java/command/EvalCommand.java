35f63a51a99578ca84dde06acf64bc1e36dd753f
Alexis <alexis.godard@telecomnancy.eu>
2026-03-16 18:16:32 +0100

    Adaptation of CLI commands to the new project structure.

--- backend/src/main/java/command/EvalCommand.java
+++ backend/src/main/java/command/EvalCommand.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
-
-import calculator.Expression;
-import calculator.Notation;
 import calculator.Calculator;
-import calculator.Divides;
+import calculator.Expression;
+import calculator.atoms.Real;
+import calculator.operations.Divides;
+import calculator.operations.Minus;
+import calculator.operations.Plus;
+import calculator.operations.Times;
 import calculator.IllegalConstruction;
-import calculator.Main;
-import calculator.Minus;
-import calculator.MyNumber;
-import calculator.Plus;
-import calculator.Times;
+import calculator.Notation;
 
 public class EvalCommand implements CLICommand {
     @Override
@@ -23,44 +21,50 @@ public class EvalCommand implements CLICommand {
             // Add call to parsing and evaluation logic
             System.out.println(args + " = ");
             
-
+            Logger logger = Logger.getLogger("EvalCommand");
 
             // ------------------OLD MAIN----------------------------------------------------
-            Expression e;
+           try {
+
             Calculator c = new Calculator();
-            Logger logger = Logger.getLogger(Main.class.getName());
+            Expression e;
 
-            
-            try{
-
-			e = new MyNumber(8);
+			e = new Real(8);
 			c.print(e);
+			c.eval(e);
 
 			List<Expression> params = new ArrayList<>();
-			Collections.addAll(params, new MyNumber(3), new MyNumber(4), new MyNumber(5));
+			Collections.addAll(params, new Real(3), new Real(4), new Real(5));
 			e = new Plus(params);
 			c.printExpressionDetails(e);
+			c.eval(e);
 
 			List<Expression> params2 = new ArrayList<>();
-			Collections.addAll(params2, new MyNumber(5), new MyNumber(3));
+			Collections.addAll(params2, new Real(5), new Real(3));
 			e = new Minus(params2);
 			c.print(e);
+			c.eval(e);
 
 			List<Expression> params3 = new ArrayList<>();
 			Collections.addAll(params3, new Plus(params), new Minus(params2));
 			e = new Times(params3);
 			c.printExpressionDetails(e);
 
+			System.out.println("PREFIX notation: " + c.format(e, Notation.PREFIX));
+			System.out.println("POSTFIX notation: " + c.format(e, Notation.POSTFIX));
+
+			c.eval(e);
+
 			List<Expression> params4 = new ArrayList<>();
-			Collections.addAll(params4, new Plus(params), new Minus(params2), new MyNumber(0));
+			Collections.addAll(params4, new Plus(params), new Minus(params2), new Real(0));
 			e = new Divides(params4);
 			c.print(e);
+			c.eval(e);
 		}
 
-		catch(IllegalConstruction _) {
+		catch (IllegalConstruction _) {
 			logger.info("cannot create operations without parameters");
-		}
-		catch(ArithmeticException error) {
+		} catch (ArithmeticException error) {
 			System.out.println("Error: " + error.getMessage() + " (NaN)");
 		}
         // ----------------------------------------------------------------------

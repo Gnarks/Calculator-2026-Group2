9d59ac43593df564665618b75f3218375e14e62f
“Nicolas <Nicolas.Verscheure@student.umons.ac.be>
2026-04-01 19:49:23 +0200

    Feat(API) : addition of tests and robustness enforced

---
+++ backend/src/test/java/calculator/TestEvaluationController.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import calculator.api.EvaluationController;
+import calculator.api.EvaluationService;
+import calculator.api.dto.EvaluationResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(EvaluationController.class)
+public class TestEvaluationController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private EvaluationService evaluationService;
+
+    @Test
+    void greetingShouldReturnMessageFromService() throws Exception {
+        EvaluationResponse response = new EvaluationResponse(1, "25");
+        when(evaluationService.evaluate("5*5")).thenReturn(response);
+        mockMvc.perform(post("/api/evaluate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"expression\":\"5*5\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value("25"))
+                .andExpect(jsonPath("$.success").value("1"));
+    }
+
+}

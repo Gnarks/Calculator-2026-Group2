a9c5f8c8f3e0b4508e38d59904f4356d477065c3
“Nicolas <Nicolas.Verscheure@student.umons.ac.be>
2026-04-03 09:56:49 +0200

    Fix(Log) : fix errors when log of integers and rationals

--- backend/src/test/java/calculator/testLog.java
+++ backend/src/test/java/calculator/testLog.java
@@ -1,11 +1,18 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import calculator.atoms.IntegerAtom;
+import calculator.atoms.Rationnal;
+import ch.obermuhlner.math.big.BigDecimalMath;
+import org.apache.commons.numbers.fraction.Fraction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import calculator.atoms.Real;
 import calculator.functions.Log;
+
+import java.math.BigDecimal;
 
 class TestLog {
 
@@ -35,5 +42,30 @@ class TestLog {
 	@Test
 	void testCompute() {
 		assertEquals(op.op(base, n), new Real(1));
+	}
+
+
+	@Test
+	void testLogWithIntegers() {
+		IntegerAtom base = new IntegerAtom(10);
+		IntegerAtom x = new IntegerAtom(100);
+		try {
+			Log log = new Log(base, x);
+			assertEquals(new Real(2), log.op(base, x));
+		} catch(IllegalConstruction _) {
+			fail();
+		}
+	}
+
+	@Test
+	void testLogWithRational() {
+		Rationnal base = new Rationnal(Fraction.of(3, 2));
+		Rationnal x = new Rationnal(Fraction.of(9));
+		try {
+			Log log = new Log(base, x);
+			assertEquals(new Real(5.4190225827029).getValue().doubleValue(), log.op(base, x).getValue().doubleValue(), 1e-9);
+		} catch(IllegalConstruction _) {
+			fail();
+		}
 	}
 }

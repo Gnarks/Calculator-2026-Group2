302dc3af601985bef466eaa0b1244d407d0f8e60
Gnarks <231026@umons.ac.be>
2026-04-03 15:27:24 +0200

    fix(TestEvalCommand): fixed logger catching error

--- backend/src/test/java/calculator/command/TestEvalCommand.java
+++ backend/src/test/java/calculator/command/TestEvalCommand.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.logging.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,20 +13,31 @@ import org.junit.jupiter.api.Test;
 class TestEvalCommand {
 
 	private final PrintStream standardOut = System.out;
+	private final PrintStream standardErr = System.err;
 	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errStreamCaptor = new ByteArrayOutputStream();
 	private EvalCommand evalCommand;
+	private StreamHandler testLogHandler;
+	private static final Logger LOGGER = Logger.getLogger(""); // Root logger to handle CLI logger
 
 	@BeforeEach
 	public void setUp() {
 		System.setOut(new PrintStream(outputStreamCaptor));
 		System.setErr(new PrintStream(errStreamCaptor));
+
+		// forcing Logger to be caught by captor
+		testLogHandler = new StreamHandler(errStreamCaptor, new SimpleFormatter());
+		testLogHandler.setLevel(Level.ALL);
+		LOGGER.addHandler(testLogHandler);
+
 		evalCommand = new EvalCommand();
 	}
 
 	@AfterEach
 	public void tearDown() {
 		System.setOut(standardOut);
+		System.setErr(standardErr);
+
 	}
 
 	@Test
@@ -53,9 +65,11 @@ class TestEvalCommand {
 	void testExecuteWithInvalidExpression() {
 
 		boolean result = evalCommand.execute("3 + * 4");
+		testLogHandler.flush();
 		assertTrue(result);
-		assertEquals(
-				"SEVERE: Invalid expression: Invalid expression at line 1:4 - extraneous input '*' expecting {'cos', 'sin', 'tan', 'acos', 'asin', 'atan', 'cosh', 'sinh', 'tanh', 'ln', 'log', 'sqrt', '(', '+', '-', 'pi', 'e', 'i', INT}",
-				errStreamCaptor.toString().split("\n")[1]);
+
+		String capturedErr = errStreamCaptor.toString();
+		assertTrue(capturedErr.contains("SEVERE"));
+		assertTrue(capturedErr.contains("Invalid expression: Invalid expression at line 1:4"));
 	}
 }

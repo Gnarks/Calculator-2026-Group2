6e2d29e20b2e85312d0159ffdf3dd1e600bc9a15
“Nicolas <Nicolas.Verscheure@student.umons.ac.be>
2026-04-10 10:58:23 +0200

    Tests(unit tests) : test coverage improvement

--- backend/src/test/java/calculator/TestIntegerAtom.java
+++ backend/src/test/java/calculator/TestIntegerAtom.java
@@ -1,10 +1,12 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import calculator.atoms.IntegerAtom;
 import calculator.atoms.Real;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import calculator.functions.Log;
+import calculator.functions.Sinus;
 
 public class TestIntegerAtom {
 
@@ -13,6 +15,29 @@ public class TestIntegerAtom {
         IntegerAtom i = new IntegerAtom(90);
         Real converted = (Real) i.toRadian();
         assertEquals(Math.PI/2, converted.getValue().doubleValue());
+    }
+
+    @Test
+    void applyUnaryFunctionOnInteger() {
+        IntegerAtom base = new IntegerAtom(2);
+        IntegerAtom i = new IntegerAtom(8);
+        try {
+            Log log = new Log(base, i);
+            assertEquals(new Real(3), base.apply(log, i));
+        } catch (IllegalConstruction _) {
+            fail();
+        }
+    }
+
+    @Test
+    void applyBinaryFunctionOnInteger() {
+        IntegerAtom i = new IntegerAtom(90);
+        try {
+            Sinus sinus = new Sinus(i);
+            assertEquals(new IntegerAtom(1), i.apply(sinus));
+        } catch (IllegalConstruction _) {
+            fail();
+        }
     }
 
 }
